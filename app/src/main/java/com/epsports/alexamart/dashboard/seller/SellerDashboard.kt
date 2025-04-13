12d7ee0f5a526ec1c6a363b75c460e52a6dfab0b
Rifat Hossain <mdrifathossain472@gmail.com>
2025-04-14 03:58:03 +0600

    add bottom navigation, app bar, app bar menu, set seller dashboad with 3 fragment

--- app/src/main/java/com/epsports/alexamart/dashboard/seller/SellerDashboard.kt
+++ app/src/main/java/com/epsports/alexamart/dashboard/seller/SellerDashboard.kt
@@ -1,28 +1,42 @@
 package com.epsports.alexamart.dashboard.seller
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.epsports.alexamart.R
 import com.epsports.alexamart.databinding.ActivitySellerDashboardBinding
+import com.epsports.alexamart.views.starter.MainActivity
+import com.google.firebase.auth.FirebaseAuth
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @Suppress("INFERRED_TYPE_VARIABLE_INTO_EMPTY_INTERSECTION_WARNING")
+@AndroidEntryPoint
 class SellerDashboard : AppCompatActivity() {
     private lateinit var binding: ActivitySellerDashboardBinding
     lateinit var navController: NavController
+    @Inject
+    lateinit var rAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivitySellerDashboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        navController = findViewById(R.id.fragmentContainerView)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
+        //navController = findNavController(R.id.fragmentContainerView)
+
         val appBarConfig = AppBarConfiguration(setOf(
             R.id.product,
             R.id.add,
@@ -31,5 +45,34 @@ class SellerDashboard : AppCompatActivity() {
 
         binding.bottomNavigationView.setupWithNavController(navController)
         setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()|| super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.seller_top_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.logout->{
+                rAuth.signOut()
+                startActivity(Intent(this@SellerDashboard, MainActivity::class.java))
+                finish()
+            }
+            R.id.report->{
+                Toast.makeText(this, "Report Clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.setting->{
+                Toast.makeText(this, "Settings Clicked", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+
+        return super.onOptionsItemSelected(item)
     }
 }
