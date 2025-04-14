1a29251dddbe2ddb6e767961dd7ae59ad9f1e2e6
Rifat Hossain <mdrifathossain472@gmail.com>
2025-04-15 03:24:01 +0600

    edit upload product fragment

--- app/src/main/java/com/epsports/alexamart/core/Extension.kt
+++ app/src/main/java/com/epsports/alexamart/core/Extension.kt
@@ -5,8 +5,6 @@ import android.widget.EditText
 import androidx.activity.result.ActivityResultLauncher
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import java.security.Permission
-import java.security.Permissions
 
 fun EditText.extract(): String {
     return text.toString().trim()
