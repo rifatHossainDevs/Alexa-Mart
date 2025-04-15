552a58565ddd6857879099d3d25a119df9d9ce19
Rifat Hossain <mdrifathossain472@gmail.com>
2025-04-15 14:03:36 +0600

    Request permission for external Storage and Camera

--- app/src/main/java/com/epsports/alexamart/base/BaseFragment.kt
+++ app/src/main/java/com/epsports/alexamart/base/BaseFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 
--- app/src/main/java/com/epsports/alexamart/dashboard/seller/upload/UploadProductFragment.kt
+++ app/src/main/java/com/epsports/alexamart/dashboard/seller/upload/UploadProductFragment.kt
@@ -1,7 +1,13 @@
 package com.epsports.alexamart.dashboard.seller.upload
 
+import android.Manifest
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.epsports.alexamart.base.BaseFragment
+import com.epsports.alexamart.core.areAllPermissionGranted
 import com.epsports.alexamart.core.extract
+import com.epsports.alexamart.core.requestPermission
 import com.epsports.alexamart.data.Product
 import com.epsports.alexamart.databinding.FragmentUploadProductBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -10,10 +16,15 @@ import dagger.hilt.android.AndroidEntryPoint
 class UploadProductFragment :
     BaseFragment<FragmentUploadProductBinding>(FragmentUploadProductBinding::inflate) {
     override fun setAllClickListener() {
+
+        permissionRequest = getPermissionRequest()
+
         binding.apply {
 
-            ivProduct.setOnClickListener {
 
+
+            ivProduct.setOnClickListener {
+                requestPermission(permissionRequest, permissionList)
             }
 
             btnUploadProduct.setOnClickListener {
@@ -35,6 +46,16 @@ class UploadProductFragment :
         }
     }
 
+    private fun getPermissionRequest(): ActivityResultLauncher<Array<String>> {
+        return registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+            if (areAllPermissionGranted(permissionList)){
+                Toast.makeText(requireContext(), "Granted", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(requireContext(), "Not Granted", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     private fun uploadProduct(product: Product) {
 
     }
@@ -43,5 +64,14 @@ class UploadProductFragment :
 
     }
 
+
+    companion object {
+        private val permissionList = arrayOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+        )
+    }
+
+    private lateinit var permissionRequest: ActivityResultLauncher<Array<String>>
 
 }
