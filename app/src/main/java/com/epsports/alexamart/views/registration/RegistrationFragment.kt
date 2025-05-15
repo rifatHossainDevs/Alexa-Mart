f0292fa4bb09085c98f6edb73046d60ba25c91f5
Rifat Hossain <mdrifathossain472@gmail.com>
2025-05-15 18:06:29 +0600

    add PushNotificationService

--- app/src/main/java/com/epsports/alexamart/views/registration/RegistrationFragment.kt
+++ app/src/main/java/com/epsports/alexamart/views/registration/RegistrationFragment.kt
@@ -33,7 +33,6 @@ class RegistrationFragment : BaseFragment<FragmentRegistrationBinding>(FragmentR
                         ""
                     )
                     viewModel.userRegistration(user)
-
                 }
             }
 
